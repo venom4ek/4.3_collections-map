17daf0ccf737d3baf0e80a4031df768bb1e3f68b
venomek <venom4ek@gmail.com>
2020-05-31 03:27:30 +0300

    added methods and tests

--- src/main/java/ru/netology/domain/FileOpenManager.java
+++ src/main/java/ru/netology/domain/FileOpenManager.java
@@ -2,7 +2,8 @@ package ru.netology.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
-import java.util.HashMap;
+
+import java.util.*;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -11,10 +12,47 @@ public class FileOpenManager {
     HashMap appsList = new HashMap<>();
 
     public void save(String ext, String app) {
-        appsList.put(ext, app);
+        appsList.put(ext.toLowerCase(), app.toLowerCase());
     }
 
     public HashMap<String, String> getAppsList() {
         return appsList;
+    }
+
+    public Object getApp(String ext) {
+        return appsList.get(ext.toLowerCase());
+    }
+
+    public Object clearApp(String ext) {
+        return appsList.replace(ext.toLowerCase(), null);
+    }
+
+    public void removeApp(String ext) {
+        appsList.remove(ext);
+    }
+
+    public List getRegisteredApp() {
+        Set<Map.Entry<String, String>> set;
+        List result = new ArrayList();
+        set = appsList.entrySet();
+        for (Map.Entry<String, String> temp : set) {
+            if (temp.getValue() != null) {
+                result.add(temp);
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FileOpenManager that = (FileOpenManager) o;
+        return Objects.equals(appsList, that.appsList);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(appsList);
     }
 }
--- src/test/java/ru/netology/domain/FileOpenManagerTest.java
+++ src/test/java/ru/netology/domain/FileOpenManagerTest.java
@@ -3,6 +3,11 @@ package ru.netology.domain;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class FileOpenManagerTest {
     private FileOpenManager openManager = new FileOpenManager();
 
@@ -11,18 +16,49 @@ class FileOpenManagerTest {
     void setUp() {
         openManager.save("jpeg", "photoshop");
         openManager.save("GIF", "photoshop");
-        openManager.save("PNG", "photoshop");
-        openManager.save("url", "chrome");
         openManager.save("mp4", "mpc-hc");
         openManager.save("mov", "mpc-hc");
-        openManager.save("avi", "mpc-hc");
-        openManager.save("mp3", "winamp");
-        openManager.save("acc", "winamp");
-        openManager.save("wav", "winamp");
+        openManager.save("html", "chrome");
+        openManager.save("chm", "chrome");
     }
 
     @Test
-    void test () {
-        System.out.println(openManager.getAppsList());
+    void test() {
+        HashMap<String, String> expected = new HashMap<>();
+        expected.put("jpeg", "photoshop");
+        expected.put("gif", "photoshop");
+        expected.put("mp4", "mpc-hc");
+        expected.put("mov", "mpc-hc");
+        expected.put("html", "chrome");
+        openManager.removeApp("chm");
+        assertEquals(expected, openManager.getAppsList());
+    }
+
+    @Test
+    void shouldRegisteredApps() {
+        openManager.clearApp("jpeg");
+        openManager.clearApp("gif");
+        openManager.clearApp("mp4");
+        List<String> expected = new ArrayList<>();
+        expected.addAll(List.of("mov=mpc-hc", "chm=chrome", "html=chrome"));
+        List<String> actual = openManager.getRegisteredApp();
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldClearBindKey() {
+        assertEquals("photoshop", openManager.clearApp("jpeg"));
+    }
+
+    @Test
+    void shouldClearBindKeyWithGetApp() {
+        openManager.clearApp("jpeg");
+        assertEquals(null, openManager.getApp("jpeg"));
+    }
+
+    @Test
+    void shouldRemoveNonExistKey() {
+        assertEquals(null, openManager.clearApp("exe"));
     }
 }

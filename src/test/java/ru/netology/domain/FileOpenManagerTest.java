3bcb2224b914d13111dcab5acc1567e7e8b73082
venomek <venom4ek@gmail.com>
2020-06-08 19:57:51 +0300

    edited FileOpenManagerTest

--- src/test/java/ru/netology/domain/FileOpenManagerTest.java
+++ src/test/java/ru/netology/domain/FileOpenManagerTest.java
@@ -9,16 +9,23 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class FileOpenManagerTest {
     private FileOpenManager openManager = new FileOpenManager();
+    private String jpeg = "jpeg";
+    private String gif = "GIF";
+    private String mp4 = "mp4";
+    private String mov = "mov";
+    private String html = "html";
+    private String chm = "chm";
+
 
 
     @BeforeEach
     void setUp() {
-        openManager.save("jpeg", "photoshop");
-        openManager.save("GIF", "photoshop");
-        openManager.save("mp4", "mpc-hc");
-        openManager.save("mov", "mpc-hc");
-        openManager.save("html", "chrome");
-        openManager.save("chm", "chrome");
+        openManager.save(jpeg, "photoshop");
+        openManager.save(gif, "photoshop");
+        openManager.save(mp4, "mpc-hc");
+        openManager.save(mov, "mpc-hc");
+        openManager.save(html, "chrome");
+        openManager.save(chm, "chrome");
     }
 
     @Test
